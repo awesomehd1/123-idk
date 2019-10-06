7f37ce7a7d7aec15b6f439894b932c306381c905
Edison <earthgood84@gmail.com>
2019-10-05 21:40:06 -0700

    Just Testing

---
+++ src/main/java/com/idiot/Main.java
@@ -0,0 +1,13 @@
+package com.idiot;
+
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        Scanner InputVariableName = new Scanner(System.in);
+        System.out.println("You	entered" + InputVariableName.nextLine());
+    }
+}
